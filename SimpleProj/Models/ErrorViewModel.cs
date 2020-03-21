1be94e3a1b22ec2cad51631283615c5f65ade201
Christian Chime <legacchris@gmail.com>
2020-03-21 11:29:10 +0100

    First Commit

---
+++ SimpleProj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
